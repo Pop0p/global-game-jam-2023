8a370566c6fb92ea0c872b456dd375a19ce92e0a
Mari-Annaig Dupaya <32541696+yukii@users.noreply.github.com>
2023-02-04 00:03:12 +0100

    Ajoute le dash

--- Assets/Scripts/Deplacement.cs
+++ Assets/Scripts/Deplacement.cs
@@ -5,22 +5,46 @@ using UnityEngine.InputSystem;
 
 public class Deplacement : MonoBehaviour
 {
-    public Vector2 movement;
+    public int Speed;
+    private Vector2 movement;
+    private bool canDash;
+    private bool canUse;
 
     // Start is called before the first frame update
     void Start()
     {
         movement = Vector2.zero;
+        canUse = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(movement.x, 0, movement.y) * Time.deltaTime);
+        transform.Translate(new Vector3(movement.x, 0, movement.y) * Time.deltaTime * Speed);
+
+        if (canDash && canUse)
+        {
+            transform.Translate(new Vector3(movement.x, 0, movement.y) * Time.deltaTime * Speed * 3);
+            canDash = false;
+            canUse = false;
+            StartCoroutine(CooldownDash());
+            Debug.Log("dash");
+        }
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         movement = context.ReadValue<Vector2>();
+    }
+
+    public void Dash(InputAction.CallbackContext context)
+    {
+        canDash = context.action.triggered;
+    }
+
+    private IEnumerator CooldownDash()
+    {
+        yield return new WaitForSeconds(3f);
+        canUse = true;
     }
 }
