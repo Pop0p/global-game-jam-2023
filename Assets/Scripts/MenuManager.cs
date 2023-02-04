fbefd16a109305cfc59b5f60b3456e2aa698c2ad
Mari-Annaig Dupaya <32541696+yukii@users.noreply.github.com>
2023-02-04 20:39:26 +0100

    Ajout scène menu début

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void OnPlay()
+    {
+        // aller dans une scène
+        SceneManager.LoadScene("Dylan");
+    }
+
+    public void OnExit()
+    {
+        // exit app
+        Application.Quit();
+    }
+}
